95f4d671802fc07cbb8abd2a57b2322fa5e2289c
McbeEringi <git@mcbeeringi.dev>
2026-05-02 08:46:42 +0900

    -

--- src/img/a.mjs
+++ src/img/a.mjs
@@ -3,7 +3,7 @@ import{renderAsync}from'@resvg/resvg-js';
 
 const
 svg=await Bun.file('icon.svg').text(),
-img=(await renderAsync(svg));
+img=(await renderAsync(svg,{fitTo:{mode:'zoom',value:2}}));
 
 await Bun.write(
 	'aspng.png',
